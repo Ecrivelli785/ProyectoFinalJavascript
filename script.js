efdebe0fca165c3ef8d9a1e1bf836705e66ad20b
enzo crivelli <enzo_crivelli97@hotmail.com>
2021-02-16 22:59:50 -0300

    localstorage and json object

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-
-
-
-    let detallesProducto = ["cantidad", "tamaño", "tintas", "papel", "familia", "gramaje", "empaquetado", "total"]
-    let caracteristicasProducto = [];
-
+   var cotizacion = {
+          id:0,
+          cantidad: "cantidad",
+          tintas: "tintas",
+          tamaño: "tamaño",
+          categoria: "categoria",
+          familia: "familia",
+          gramaje: "gramaje",
+          empaquetado: "empaquetado",
+          estado: 0
+    };
+    var carrito = [cotizacion];
+    function asignarValor(lista,valorABuscar, valorAsignar) {
+        lista.forEach(function (elemento) { // recorremos el array
+            //asignamos el valor del elemento dependiendo del valor a buscar, validamos que el valor sea el mismo y se reemplaza con el nuevo.
+            elemento[valorABuscar] = valorAsignar;
+        })
+    };
     function operacion() {
         caja = document.forms["sumar"].elements;
-        var cantidad1 = Number(caja["cantidad"].value);
-        var tamaño1 = Number(caja["tamaño"].value);
-        var tintas1 = Number(caja["tintas"].value);
-        var papel1 = Number(caja["papel"].value);
-        var familia1 = Number(caja["familia"].value);
-        var gramaje1 = Number(caja["gramaje"].value);
-        var empaquetado1 = Number(caja["empaquetado"].value);
-        var total = (cantidad1 * (tamaño1 * (papel1 * familia1 * gramaje1))) + tintas1 + empaquetado1;
+        cotizacion.cantidad = Number(caja["cantidad"].value);
+        cotizacion.tintas = Number(caja["tintas"].value);
+        cotizacion.tamaño = Number(caja["tamaño"].value);
+        cotizacion.categoria = Number(caja["categoria"].value);
+        cotizacion.familia = Number(caja["familia"].value);
+        cotizacion.gramaje = Number(caja["gramaje"].value);
+        cotizacion.empaquetado = Number(caja["empaquetado"].value);
+        console.log(cotizacion);
+        var total = (cotizacion.cantidad * cotizacion.tamaño * cotizacion.categoria * cotizacion.familia * cotizacion.gramaje) + cotizacion.tintas + cotizacion.empaquetado;
+        //var total = (cantidad1 * (tamaño1 * (categoria1 * familia1 * gramaje1))) + tintas1 + empaquetado1;
         var totalImpuestos = total * 1.21;
         document.getElementById('total').value = total;
         document.getElementById('totalImpuestos').value = totalImpuestos;
 
-        caracteristicasProducto.push(cantidad1, tamaño1, tintas1, papel1, familia1, empaquetado1, total)
 
-        var show = (detallesProducto[0].concat(caracteristicasProducto[0]));
+
+        // Guardo el objeto como un string
+        localStorage.setItem('datos', JSON.stringify(cotizacion));
+        // Obtengo el string previamente salvado y luego
+        var guardado = localStorage.getItem('datos');
+        console.log('objetoObtenido1: ', JSON.parse(guardado));
     };
-
-
-
-
-var AgregarCarrito = function(){
-        let datosTrabajo = document.getElementsByClassName("infoPresupuesto"),
-            carrito = [];
-        for (var i = 0; i < datosTrabajo.length; i++) {
-            carrito[i] = datosTrabajo[i].value;
-            console.log (datosTrabajo[i].value);
-            }
-        }
+    $('btn-agregarcarrito').click(function(){
+        cotizacion.estado = "1";
+    });
+//    function agregarcarrito() {
+  //      if (carrito.length )
+    //    cotizacion.id = carrito.length+1;
+    //}
